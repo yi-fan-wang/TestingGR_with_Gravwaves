1b9fcd36567801a2fed830cc0a74c93fe2bea655
Yifan Wang <yifan.wang@aei.mpg.de>
2023-08-02 16:40:19 +0000

    fix bugs;add examples

--- pytgr/fta.py
+++ pytgr/fta.py
@@ -31,6 +31,8 @@ def gen_waveform(**kwds):
 
     nonGRdict = lal.CreateDict()
     if kwds['ftadchi2'] != None:
+        if kwds['ftadchi2'] == 0:
+            return hp, hc
         lalsim.SimInspiralWaveformParamsInsertNonGRDChi2(nonGRdict,float(kwds['ftadchi2']))
     
     hplal = hp.lal()
@@ -38,31 +40,31 @@ def gen_waveform(**kwds):
     # add FTA correction
     lalsim.SimInspiralTestingGRCorrections(hplal,
                                        2,2, #only support (2,2) mode
-                                       par['mass1']*lal.MSUN_SI,
-                                       par['mass2']*lal.MSUN_SI, 
-                                       par['spin1z'], 
-                                       par['spin2z'], 
-                                       par['f_lower'],#f_start, 
-                                       par['f_lower'],#f_ref, 
+                                       kwds['mass1']*lal.MSUN_SI,
+                                       kwds['mass2']*lal.MSUN_SI, 
+                                       kwds['spin1z'], 
+                                       kwds['spin2z'], 
+                                       kwds['f_lower'],#f_start, 
+                                       kwds['f_lower'],#f_ref, 
                                        0.39, #https://arxiv.org/pdf/2109.06988.pdf 
                                        1,
                                        nonGRdict)
 
     lalsim.SimInspiralTestingGRCorrections(hclal,
                                        2,2,
-                                       par['mass1']*lal.MSUN_SI,
-                                       par['mass2']*lal.MSUN_SI, 
-                                       par['spin1z'], 
-                                       par['spin2z'], 
-                                       par['f_lower'],#f_start, 
-                                       par['f_lower'],#f_ref, 
+                                       kwds['mass1']*lal.MSUN_SI,
+                                       kwds['mass2']*lal.MSUN_SI, 
+                                       kwds['spin1z'], 
+                                       kwds['spin2z'], 
+                                       kwds['f_lower'],#f_start, 
+                                       kwds['f_lower'],#f_ref, 
                                        0.39, #https://arxiv.org/pdf/2109.06988.pdf 
                                        1,
                                        nonGRdict)
     
     # build FrequencySeries format
-    hpfta = FrequencySeries(hplal.data.data, dtype=hplus.data.data.dtype,
+    hpfta = FrequencySeries(hplal.data.data, dtype=hplal.data.data.dtype,
                             delta_f=hplal.deltaF, epoch=hplal.epoch)
-    hcfta = FrequencySeries(hclal.data.data, dtype=hcross.data.data.dtype,
+    hcfta = FrequencySeries(hclal.data.data, dtype=hclal.data.data.dtype,
                             delta_f=hplal.deltaF, epoch=hclal.epoch)
     return hpfta, hcfta
