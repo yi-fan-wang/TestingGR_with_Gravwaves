39e097b93e41c083b85abfce1340c75baf8e4219
Yifan Wang <yifan.wang@aei.mpg.de>
2023-08-11 15:50:45 +0200

    add ppe

---
+++ pytgr/ppe.py
@@ -0,0 +1,38 @@
+
+def gen_waveform(**kwds):
+    '''
+    Parameterized Post Einstein
+
+    Parameters
+    ----------
+    kwds: dict
+        Only support ppedchi2 atm.
+
+    Returns
+    -------
+    hp: pycbc.types.FrequencySeries
+        Plus polarization time series
+    hc: pycbc.types.FrequencySeries
+        Cross polarization time series
+    '''
+    from pycbc.waveform import get_fd_waveform
+
+    # sanity checks
+    if kwds['baseapprox'] is None:
+        raise ValueError("A base waveform approximant is required.")
+    # Generate GR waveforms
+    if 'approximant' in kwds:
+        kwds.pop("approximant")
+    hp, hc = get_fd_waveform(approximant=kwds['baseapprox'], **kwds)
+
+    # Add PPE parameters
+    if kwds['ppedchi2'] != None:
+        if kwds['ppedchi2'] == 0:
+            return hp, hc
+        else:
+            phi = kwds['ppedchi2'] / hp.sample_frequencies[1:]
+            # slicing with index 1 to avoid dividing zero frequency
+
+    hp[1:] *= numpy.exp(-1j*phi)
+    hc[1:] *= numpy.exp(-1j*phi)
+    return hp, hc
