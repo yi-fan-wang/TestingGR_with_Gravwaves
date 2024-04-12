955f3f0ba23b73105257fda85f7c4798556f749c
Yifan Wang <yifan.wang@aei.mpg.de>
2024-04-12 16:32:44 +0200

    correct typos

--- pytgr/lineofsight.py
+++ pytgr/lineofsight.py
@@ -1,16 +1,15 @@
-import numpy
+
 
 def gen_waveform(**kwds):
     from pycbc.waveform import get_fd_waveform
-    from pycbc import cosmology
-    import lal.LAL_MTSUN_SI
+    import lal, numpy
     from pycbc import conversions
 
     if 'approximant' in kwds:
         kwds.pop("approximant")
     if kwds['baseapprox'] is None:
         raise ValueError("A base waveform approximant is required.")
-    if kwds['lsc_a'] is None:
+    if kwds['lsa_a'] is None:
         raise ValueError("A line of sight acceleration parameter is required!")
 
     hp, hc = get_fd_waveform(approximant=kwds['baseapprox'], **kwds)
@@ -18,10 +17,10 @@ def gen_waveform(**kwds):
 
     eta = conversions.eta_from_mass1_mass2(kwds['mass1'],kwds['mass2'])
     m_total = kwds['mass1'] + kwds['mass2']
-    v_f = np.pi * lal.LAL_MTSUN_SI * m_total * fseries
-    delta_phi = 25 / 65536 / eta / eta * lal.LAL_MTSUN_SI * m_total * kwds['lsc_a'] * vf ** (-13)
+    v_f = lal.PI * lal.MTSUN_SI * m_total * fseries[1:]
+    delta_phi = 25 / 65536 / eta / eta * lal.MTSUN_SI * m_total * kwds['lsa_a'] / lal.C_SI * v_f ** (-13)
 
-    hp_lsa = hp * delta_phi
-    hc_lsa = hc * delta_phi
+    hp_lsa = hp[1:] * numpy.exp(1j * delta_phi)
+    hc_lsa = hc[1:] * numpy.exp(1j * delta_phi)
 
     return hp_lsa, hc_lsa
